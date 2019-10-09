12273f6bb82ebedc4737d0c872a88d7accd80be6
Giovanni Flores <geflores@Pip-Boy.local>
2019-10-08 22:16:58 -0700

    basic nav setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Giovanni Flores`,
+    description: `Portfolio`,
+    author: `Giovanni Flores`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -13,6 +13,7 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    'gatsby-plugin-sass',
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
@@ -27,6 +28,21 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          'gatsby-remark-relative-images',
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 750,
+              linkImagesToOriginal: false
+            }
+          }
+        ]
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '../components/layout';
+import Head from '../components/head';
+
+
+const AboutPage = () => (
+    <Layout>
+        <Head title="About" />
+        <h1>About Me</h1>
+        <p>Web Developer</p>
+    </Layout>
+)
+
+export default AboutPage;
