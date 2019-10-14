c78b549d72e81c6fbfe31bf9a6e2f430a3c85154
Giovanni Flores <geflores@Pip-Boy.local>
2019-10-13 21:36:08 -0700

    add basic info to contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,13 +2,19 @@ import React from 'react';
 import Layout from '../components/layout';
 import Head from '../components/head';
 
+const ContactPage = () => {
+    return (
+        <Layout>
+            <Head title="Contact" />
+            <h1>Connect with Me</h1>
+            <p>Email: geflores5@gmail.com</p>
+            <p>
+                <a href="https://github.com/geflores5" target="_blank" >
+                    github
+                </a>
+            </p>
+        </Layout>
+    );
+}
 
-const AboutPage = () => (
-    <Layout>
-        <Head title="About" />
-        <h1>About Me</h1>
-        <p>Web Developer</p>
-    </Layout>
-)
-
-export default AboutPage;
+export default ContactPage;
