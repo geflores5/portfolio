c03705f587bfff22428cdfe9f355938b1d532d11
Giovanni Flores <geflores5@gmail.com>
2021-02-23 17:43:38 -0800

    Switch to single project on each row, still need css changes

--- src/components/projects.js
+++ src/components/projects.js
@@ -11,7 +11,7 @@ import projectsStyles from './projects.module.scss'
 
 const Projects = () => {
   return (
-    <div>
+    <div className={projectsStyles.container}>
       <div className={projectsStyles.row}>
         <div className={projectsStyles.project}>
           <a
@@ -21,32 +21,38 @@ const Projects = () => {
           >
             <img src={inkrprate} alt="Inkrprate" />
           </a>
-          <p>MongoDB | Express | React | Node | Context | Firebase</p>
-          <ul className={projectsStyles.info}>
-            <li>Implemented user authentication using Firebase, allowing users to signup, login, and reset password</li>
-            <li>Utilized React’s Context API to allow app wide access to currently logged in user info and the implementation of protected routes and user data</li>
-          </ul>
+          <div>
+            <p>MongoDB | Express | React | Node | Context | Firebase</p>
+            <ul className={projectsStyles.info}>
+              <li>Implemented user authentication using Firebase, allowing users to signup, login, and reset password</li>
+              <li>Utilized React’s Context API to allow app wide access to currently logged in user info and the implementation of protected routes and user data</li>
+            </ul>
+          </div>
         </div>
-        <div className={projectsStyles.project}>
-          <a
-            href="https://target-related-items.herokuapp.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={related} alt="Related Items" />
-          </a>
-          <p>MongoDB | Express | React | Node | AWS EC2 | Docker</p>
-          <ul className={projectsStyles.info}>
-            <li>Dynamically renders content allowing users to horizontally scroll through a carousel of related items using raw CSS</li>
-            <li>Dockerized and hosted on AWS EC2 instance</li>
-          </ul>
-          <a
-            href="https://github.com/TeamM-V-P/related-items"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={gitcat} alt="geflores5" />
-          </a>
+        <div className={projectsStyles.row} >
+          <div className={projectsStyles.project}>
+            <a
+              href="https://target-related-items.herokuapp.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={related} alt="Related Items" />
+            </a>
+            <div>
+              <p>MongoDB | Express | React | Node | AWS EC2 | Docker</p>
+              <ul className={projectsStyles.info}>
+                <li>Dynamically renders content allowing users to horizontally scroll through a carousel of related items using raw CSS</li>
+                <li>Dockerized and hosted on AWS EC2 instance</li>
+              </ul>
+              <a
+                href="https://github.com/TeamM-V-P/related-items"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={gitcat} alt="geflores5" />
+              </a>
+            </div>
+          </div>
         </div>
       </div>
       <div className={projectsStyles.row}>
@@ -58,21 +64,25 @@ const Projects = () => {
           >
             <img src={remember} alt="Remember When" />
           </a>
-          <p>React | Redux | Firebase Auth/DB/Hosting</p>
-          <ul className={projectsStyles.info}>
-            <li>Users create timelines and input memories</li>
-            <li>Upload images and add description and date</li>
-            <li>Sort by date or filter by keyword</li>
-            <li>User authentication through email and password</li>
-          </ul>
-          <a
-            href="https://github.com/geflores5/remember-when"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={gitcat} alt="geflores5" />
-          </a>
+          <div>
+            <p>React | Redux | Firebase Auth/DB/Hosting</p>
+            <ul className={projectsStyles.info}>
+              <li>Users create timelines and input memories</li>
+              <li>Upload images and add description and date</li>
+              <li>Sort by date or filter by keyword</li>
+              <li>User authentication through email and password</li>
+            </ul>
+            <a
+              href="https://github.com/geflores5/remember-when"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={gitcat} alt="geflores5" />
+            </a>
+          </div>
         </div>
+      </div>
+      <div className={projectsStyles.row} >
         <div className={projectsStyles.project}>
           <div
             className="video"
@@ -88,8 +98,8 @@ const Projects = () => {
                 position: "absolute",
                 top: 0,
                 left: 0,
-                width: "100%",
-                height: "100%",
+                width: "500px",
+                height: "300px",
               }}
               src="https://www.youtube.com/embed/4dxDV7h4p8c"
               frameBorder="0"
@@ -97,18 +107,20 @@ const Projects = () => {
               title="remember-when"
             />
           </div>
-          <p>React Native | Redux | Firebase Auth/DB</p>
-          <ul className={projectsStyles.info}>
-            <li>React Native app version of site</li>
-            <li>Real time connection through Firebase</li>
-          </ul>
-          <a
-            href="https://github.com/geflores5/remember-when-native"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={gitcat} alt="geflores5" />
-          </a>
+          <div>
+            <p>React Native | Redux | Firebase Auth/DB</p>
+            <ul className={projectsStyles.info}>
+              <li>React Native app version of site</li>
+              <li>Real time connection through Firebase</li>
+            </ul>
+            <a
+              href="https://github.com/geflores5/remember-when-native"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={gitcat} alt="geflores5" />
+            </a>
+          </div>
         </div>
       </div>
       <div className={projectsStyles.row}>
@@ -120,19 +132,23 @@ const Projects = () => {
           >
             <img src={weather} alt="Node Weather App" />
           </a>
-          <p>Node | Express | DarkSky API | Heroku</p>
-          <ul className={projectsStyles.info}>
-            <li>Weather app utilizing Node asyncronous programming</li>
-            <li>Retrieves data from mapbox.com and darksky.net</li>
-          </ul>
-          <a
-            href="https://github.com/geflores5/node-weather-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={gitcat} alt="geflores5" />
-          </a>
+          <div>
+            <p>Node | Express | DarkSky API | Heroku</p>
+            <ul className={projectsStyles.info}>
+              <li>Weather app utilizing Node asyncronous programming</li>
+              <li>Retrieves data from mapbox.com and darksky.net</li>
+            </ul>
+            <a
+              href="https://github.com/geflores5/node-weather-app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={gitcat} alt="geflores5" />
+            </a>
+          </div>
         </div>
+      </div>
+      <div className={projectsStyles.row} >
         <div className={projectsStyles.project}>
           <a
             href="https://exercise-tracker-geflores5.herokuapp.com/"
@@ -141,18 +157,20 @@ const Projects = () => {
           >
             <img src={exertracker} alt="Exercise Tracker" />
           </a>
-          <p>MongoDB | Node | Express | Heroku</p>
-          <ul className={projectsStyles.info}>
-            <li>Simple Exercise Tracker with crud functionality</li>
-            <li>Add users/exercises, update/delete exercises</li>
-          </ul>
-          <a
-            href="https://github.com/geflores5/exertracker-api"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={gitcat} alt="geflores5" />
-          </a>
+          <div>
+            <p>MongoDB | Node | Express | Heroku</p>
+            <ul className={projectsStyles.info}>
+              <li>Simple Exercise Tracker with crud functionality</li>
+              <li>Add users/exercises, update/delete exercises</li>
+            </ul>
+            <a
+              href="https://github.com/geflores5/exertracker-api"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={gitcat} alt="geflores5" />
+            </a>
+          </div>
         </div>
       </div>
       <div className={projectsStyles.row}>
@@ -164,19 +182,23 @@ const Projects = () => {
           >
             <img src={githubFinder} alt="Github Finder" />
           </a>
-          <p>React | GitHub API | Context | Netlify</p>
-          <ul className={projectsStyles.info}>
-            <li>Simple GitHub profile finder using GitHub API</li>
-            <li>Search users by name and view basic profile information</li>
-          </ul>
-          <a
-            href="https://github.com/geflores5/github-finder"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={gitcat} alt="geflores5" />
-          </a>
+          <div>
+            <p>React | GitHub API | Context | Netlify</p>
+              <ul className={projectsStyles.info}>
+                <li>Simple GitHub profile finder using GitHub API</li>
+                <li>Search users by name and view basic profile information</li>
+              </ul>
+              <a
+                href="https://github.com/geflores5/github-finder"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={gitcat} alt="geflores5" />
+              </a>
+          </div>
         </div>
+      </div>
+      <div className={projectsStyles.row} >
         <div className={projectsStyles.project}>
           <a
             href="https://geflores5-gatsby-blog.netlify.com/"
@@ -185,18 +207,20 @@ const Projects = () => {
           >
             <img src={blog} alt="Gatsby Blog" />
           </a>
-          <p>React | Gatsby | Contentful | Netlify</p>
-          <ul className={projectsStyles.info}>
-            <li>Blog with content managed using Contentful</li>
-            <li>Disqus commenting enabled</li>
-          </ul>
-          <a
-            href="https://github.com/geflores5/gatsby-blog"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={gitcat} alt="geflores5" />
-          </a>
+          <div>
+            <p>React | Gatsby | Contentful | Netlify</p>
+            <ul className={projectsStyles.info}>
+              <li>Blog with content managed using Contentful</li>
+              <li>Disqus commenting enabled</li>
+            </ul>
+            <a
+              href="https://github.com/geflores5/gatsby-blog"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={gitcat} alt="geflores5" />
+            </a>
+          </div>
         </div>
       </div>
     </div>
